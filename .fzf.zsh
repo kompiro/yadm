411e2fb79525583a197ade249bb57461fdfcce59
Hiroki Kondo <kompiro@gmail.com>
2024-06-19 15:30:35 +0000

    feat: Update version

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "${HOME}/.asdf/installs/fzf/0.34.0/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "${HOME}/.asdf/installs/fzf/0.53.0/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "${HOME}/.asdf/installs/fzf/0.34.0/shell/key-bindings.zsh"
+source "${HOME}/.asdf/installs/fzf/0.53.0/shell/key-bindings.zsh"
